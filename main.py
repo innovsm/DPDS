df8ef6e93387c051b5f632e045f4677ff293c57f
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-09-13 12:22:00 +0530

    final deployment of app

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import streamlit as st 
 from gpt_connection import chat_with_chatgpt
-
-
 # main code comes here
-st.header("Descriva")
-# user - type ---> 
-user_type = st.text_input("Enter the Customer profession","i.e Student, Businessman, etc")
+
+st.header("Demo E-commerce platform")
+# putting one dropdown here
+with st.expander(label = "how app works",expanded=False):
+    st.write("""This app uses customer order history and personal profile to provide targeted  description of the
+             product user is searching for. This technology increases the consumer engagment and likliehod of consumer buying the 
+             product. The reason is personalized product description and not the general one , giving the feel 
+             that product is meant for the customer
+             """)
+
+user_type = st.text_input("Enter the Customer profession, i.e , doctor , student, None") # info - 1
+order_history = st.text_input("Enter user order hstory i.e , books, markers, shoes , t-shirt")  # info - 2
+
+
+
 user_input =  st.selectbox("Select Product", ['', '5 AM CLUB BOOK','Smart Watch','Laptop','Protein Powder',
                                               'Shoes','Peanut Butter'])
 run_button =   st.button("Run")
 # sidebar
 with st.sidebar:
-    st.subheader("About Internal Working")
-    st.write("Imagine exploring a product, and within moments, the description mirrors your interests, uses your preferred tone, and highlights features that matter to you most. This personal touch is made possible by our advanced AI, which constantly learns from your interactions and feedback So, how does it work? It's simple yet powerful: Descriva listens to your digital cues, understands your unique needs, and crafts descriptions that speak directly to you. With Descriva, your online shopping experience is not just personalized; it's an enjoyable journey designed with you in mind.")
-
+    st.subheader("Steps to Use the app")
+    st.write("1. Enter the user profession , or leave is blank")
+    st.write("2. Enter the user previous order history")
+    st.write("3. select the demo product")
+    # result
+    st.subheader("Result")
+    st.write("A PERSONALIZED PRODUCT DESCRIPTION CAN BE SEEN !!!")
 # inserting image
 col1 , col2, col3 = st.columns([3,4,2])
 
 
-if(run_button and user_input != "" and user_type  != ""):
+if(run_button and user_input != "" and order_history  != ""):
+    print(user_input, order_history)
     # inserting image here
     product_details = "" 
     if(user_input == "5 AM CLUB BOOK"):
@@ -55,10 +70,8 @@ if(run_button and user_input != "" and user_type  != ""):
 
     
     # framing the querry for result
-    query = "Write a  effictive and engaging description under 200 words product description   of {} our target customer are {}, product details are {}".format(user_input, user_type,product_details)
-
+    query = "Write a  effictive and engaging description under 200 words product description   of {} our target customer are {}, product details are {}  , user had order history of {}".format(user_input, user_type,product_details, product_details)
+    #        Write a  effictive and engaging description under 200 words product description , of Peanut Butter our main customers are old people, product details are MYFITNESS Chocolate Peanut Butter Smooth 1250g | 22g Protein | Tasty & Healthy Nut Butter Spread |Vegan| Dark Chocolate | Cholesterol Free, Gluten Free| Smooth Peanut Butter | Zero Trans Fat
     data = chat_with_chatgpt(query)
     st.write(data)
-
-    st.button("Buy")
-
+    st.button("buy")
